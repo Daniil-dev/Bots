5020f90c033d858665ec74c3232a758ee6926a31
Daniil-dev <60258376+Daniil-dev@users.noreply.github.com>
2020-12-25 19:02:36 +0300

    Update DataBase.java

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -2,50 +2,13 @@ import java.sql.*;
 
 public class DataBase {
     //DESKTOP-0HJNGIA\MSSQL  price_tag
-    private final String dataBase = "HealthBot";
-    private final String url      = "jdbc:sqlserver://DESKTOP-0HJNGIA\\MSSQL;databaseName=" + dataBase;
-    private final String user     = "sa";
-    private final String password = "1234";
+    private final String dataBase = "Name your database";
+    private final String url      = "your link to the database" + dataBase;
+    private final String user     = "User";
+    private final String password = "Password";
 
     private Connection connection;
     private Statement  statement;
-
-    /*public BD() {
-        try {
-            // создаем что-то нужное
-            connection = DriverManager.getConnection(url, user, password);
-            // создаем что-то нужное из чего-то нужного
-            Statement statement = connection.createStatement();
-
-
-            // пример кода для вставки новой строки в таблицу category
-//          statement.executeUpdate("INSERT INTO category (name) VALUES ('THE TEST')");
-
-
-            // вызываем метод executeQuery из объекта statement и передаем строку с SQL запросом
-            // результат выполнения записываем в переменную result
-            ResultSet result = statement.executeQuery("SELECT * FROM category where id=5");
-
-            // перебераем внутри цикла объект result. с помощью метода getString() получаем строку в указаном столбце по имени или индексу
-            // В.А.Ж.Н.О!!! если не использовать метод "next()" то просто взять результат(строку) не получиться даже если она одна
-            // изначально значение - это 0 любой результат находиться начиная с 1 строки
-            while(result.next()){
-                String name = result.getString("name");
-                System.out.println(name);
-            }
-
-
-            // пример вывода значения БЕЗ использование цикла
-            result = statement.executeQuery("SELECT * FROM category where id=7");
-            result.next();
-            System.out.println(result.getString("name"));
-
-
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }*/
 
     public void openConnection(){
         try {
